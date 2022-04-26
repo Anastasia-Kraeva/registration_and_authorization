b27e5103d923d81ec7b93addaf61aee3b0d2e4a3
Anastasia811625 <86957944+Anastasia811625@users.noreply.github.com>
2022-04-26 09:14:06 +0300

    created registration(), handleSubmit(); changed props naiming

--- src/pages/Register.page.tsx
+++ src/pages/Register.page.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import {useDispatch} from 'react-redux';
 import {Container} from '@mui/material';
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom';
+import {useSearchParams} from 'react-router-dom';
 
+import {BASE_URL} from '../constants/constants';
+import * as actions from '../store/actions/actionCreaters';
 import {Form} from '../components/form/Form';
 
 export const RegisterPage = () => {
-  const formData = {
+  const formRenderingData = {
     formHeader: 'Вход в систему',
     fieldsList: [
       {
-        name: 'nickname',
+        name: 'username',
         label: 'Уникальный никнейм',
       },
       {
@@ -16,29 +22,52 @@ export const RegisterPage = () => {
         label: 'E-mail',
       },
       {
-        name: 'password',
+        name: 'password1',
         label: 'Пароль',
         type: 'password',
       },
       {
-        name: 'passwordConfirmation',
+        name: 'password2',
         label: 'Подтверждение пароля',
         type: 'password',
       },
       {
-        name: 'checkword',
+        name: 'keyword',
         label: 'Контрольное слово',
       },
     ],
     formButtonText: 'Зарегистрироваться',
-    handleSubmit: (formData:any) => {
-      console.log('submit', formData)
+    handleSubmit: handleSubmit,
+  }
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+
+  const registration = async (data: any) => {
+    try {
+      const response = await axios.post(`${BASE_URL}/register/`, data)
+      console.log(response)
+      return response.data
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  async function handleSubmit(formData: any) {
+    try {
+      console.log('submit', formData);
+      // const userData = await registration(formData)
+      // localStorage.setItem('user', JSON.stringify({data: userData}));
+      // dispatch(actions.addUser(userData));
+      navigate(`/auth`)
+    } catch (e) {
+      console.log(e)
     }
   }
 
   return (
-    <Container maxWidth='xs'>
-      <Form {...formData}/>
+    <Container maxWidth="xs">
+      <Form {...formRenderingData}/>
     </Container>
   )
 }
