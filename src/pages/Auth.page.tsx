8611f9bbf0f316dd6c1cb1c33d8a521be48d1730
Anastasia811625 <86957944+Anastasia811625@users.noreply.github.com>
2022-04-27 07:02:12 +0300

    changed AuthPage(changed props naming); created tokenSlice, preLogin(), login(), handleSubmit()

--- src/pages/Auth.page.tsx
+++ src/pages/Auth.page.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useDispatch} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
 import {Container} from '@mui/material';
 
-import {useTypedSelector} from '../hooks/typedSelector';
+import {BASE_URL} from '../constants/constants';
 import {Form} from '../components/form/Form';
+import * as actions from '../store/actions/actionCreaters';
 
 export const AuthPage = () => {
-  // todo
-  // const {isAccess} = useTypedSelector(state => state.user.isAccess)
-  const isAccess = false
+  const [isAccess, setIsAccess] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const accessField = isAccess
     ? {
       name: 'confirmationCode',
       label: 'Код подтверждения, полученный по e-mail',
     }
     : {
-      name: 'password',
+      name: 'password1',
       label: 'Пароль',
       type: 'password',
     }
-  const formData = {
+  const formRenderingData = {
     formHeader: 'Вход в систему',
     fieldsList: [
       {
@@ -28,14 +32,58 @@ export const AuthPage = () => {
       accessField,
     ],
     formButtonText: 'Войти',
-    handleSubmit: (formData: any) => {
-      console.log('submit', formData)
+    handleSubmit: handleSubmit,
+  }
+
+  const preLogin = async (data: any) => {
+    try {
+      // const response = await axios.post(`${BASE_URL}/pre-login/`, data);
+      // console.log('response', response);
+      setIsAccess(true)
+      // return response.data;
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const login = async (data: any) => {
+    try {
+      const response = await axios.post(`${BASE_URL}/login/`, data)
+      console.log('login', response)
+      return response.data
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  function handleSubmit(data: any): any {
+    console.log('isAccess', isAccess)
+    if (!isAccess) {
+      const preLoginData = {
+        login: data.email,
+        password: data.password1,
+      }
+      console.log('submit', preLoginData);
+      preLogin(preLoginData);
+    } else {
+      const formData = {
+        code: data.confirmationCode,
+        email: data.email,
+      }
+      console.log('submit', formData);
+      // const token = login(formData)
+      const token = {
+          refresh: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY1MTA1OTgxOCwiaWF0IjoxNjUwOTczNDE4LCJqdGkiOiI3MTI2NjVjMzk1YTQ0MWQyYjJlMThhNDczNjIwNjMxOCIsInVzZXJfaWQiOjExN30.N2WLA4k62XeZo-S6pQhjI0srymvvMlahnN9ie4a92ZQ',
+          access: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjUwOTc0NjE4LCJpYXQiOjE2NTA5NzM0MTgsImp0aSI6IjQ4MTNhNTZmMjQ4ODQxOTNhYTM0YTE2YzIyZThhNmMxIiwidXNlcl9pZCI6MTE3fQ.g2DNPly4fOVkchrKUe0sQAZcs-SlvxSHEB2KsUAd6TA'
+        }
+      dispatch(actions.login(token));
+      navigate(`/`)
     }
   }
 
   return (
     <Container maxWidth="xs">
-      <Form {...formData}/>
+      <Form {...formRenderingData}/>
     </Container>
   )
 }
---
+++ src/store/reducers/tokenSlice.ts
@@ -0,0 +1,27 @@
+// @ts-nocheck
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {IAppState, userType} from './types';
+import * as actions from '../actions/actionCreaters';
+
+const initialState: IAppState = JSON.parse(localStorage.getItem('token')) || {}
+
+export const tokenSlice = createSlice({
+  name: 'token',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(actions.login, (state, action: PayloadAction<any>) => {
+        Object.keys(action.payload)
+          .forEach((key: any) => {
+            state[key] = action.payload[key]
+          })
+      })
+    .addCase(actions.logout, (state, action: PayloadAction<userType>) => {
+      console.log('token logout')
+      state = {};
+    })
+  }
+})
+
+export default tokenSlice.reducer;
